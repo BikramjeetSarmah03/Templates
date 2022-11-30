c0850309a086d32d1a141a1175b615d2d5c56792
Bikramjeet Sarmah <9854bikram@gmail.com>
2022-11-30 20:27:33 +0530

    Strapi

---
+++ React Native & Tailwind/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text class="bg-red-300">Hello</Text>
+    </View>
+  );
+};
+
+export default App;
